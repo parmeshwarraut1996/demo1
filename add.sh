bd52beb493683232cee28cfb267ab0efae291a78
parmeshwarraut1996 <parmeshwarraut1996@gmail.com>
2021-02-09 13:52:23 +0530

    add mul

--- add.sh
+++ add.sh
@@ -3,12 +3,15 @@
 
 add;
 sub;
+mul;
 
 read -p "Enter the value of num1 " c
 read -p "Enter the value of " b
 
 add=$(( $c + $b ))
 sub=$(( $c + $b ))
+mul=$(( $c * $b ))
 
 echo "sum= $add"
 echo "sub= $sub"
+echo "mul= $mul"
