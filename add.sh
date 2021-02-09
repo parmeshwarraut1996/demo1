b45af990c7d6dac37c0e139673e23a0e8bc53eee
parmeshwarraut1996 <parmeshwarraut1996@gmail.com>
2021-02-09 13:33:31 +0530

    add sub

--- add.sh
+++ add.sh
@@ -2,12 +2,13 @@
 
 
 add;
-
+sub;
 
 read -p "Enter the value of num1 " c
 read -p "Enter the value of " b
 
-add=$(( $e + $b ))
-
+add=$(( $c + $b ))
+sub=$(( $c + $b ))
 
 echo "sum= $add"
+echo "sub= $sub"
